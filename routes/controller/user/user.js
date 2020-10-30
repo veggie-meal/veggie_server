2207fa156711f9ce640adf2b5ae046e98b1a98c8
eclat <enrichir97@gmail.com>
2020-10-31 01:44:02 +0900

    ### 1.  충돌 해결

--- routes/controller/user/user.js
+++ routes/controller/user/user.js
@@ -3,44 +3,7 @@ const db =require("../../../loader/db");
 const userService = require("../../service/user/userService");
 
 class User {
-  login(req, res) {
-    console.log("login");
-    let data = req.body;
-
-
-<<<<<<< HEAD
-    kakao(data.userAT, (result, resultObj) => {
-      if (!result) {
-        console.log(resultObj);
-        return res.json({
-          result: false,
-          code: -1,
-        });
-      }
-
-      userService.findUserId(data.userId, (result, userObj) => {
-        if (!result)
-          return res.json({
-            result: false,
-            code: 0,
-          });
-
-        if (userObj.length > 0)
-          return res.json({
-            result: true,
-            code: 1,
-          });
-        else if (userObj.length == 0)
-          return res.json({
-            result: true,
-            code: 2,
-          });
-      });
-    });
-  }
-
-  /*
-=======
+ 
     login(req,res){
         console.log("login");
         let data = req.body;
@@ -93,7 +56,6 @@ class User {
 
 
     /*
->>>>>>> 0606be8d5944337ec159d21e837dfd964ea43e73
 
         목표 입력 함수
 
@@ -112,7 +74,7 @@ class User {
   //유저 정보 들고오기
   userData(req, res) {
     let data = req.body;
-    
+
     userService.findUserId(data.userId, (result, resultObj) => {
       return res.json({
         result: result,
